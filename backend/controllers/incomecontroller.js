291227ef58081d73b58a36d6bbfe1a6d56c1349e
shreyaupd <shreyaupd234@gmail.com>
2025-07-16 09:31:50 +0545

    implement income model

---
+++ backend/controllers/incomecontroller.js
@@ -0,0 +1,14 @@
+import Income from '../models/income.js';
+import User from '../models/user.js';
+const addincome = async(req, res)=>{
+
+}
+const getincome = async(req, res)=>{
+    
+}
+const deleteincome = async(req, res)=>{
+    
+}
+const downloadincome = async(req, res)=>{
+    
+}
